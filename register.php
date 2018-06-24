0c1588e281cf018a4b0009c45fb8413eebbd0d3d
eltejedor <eltejedor@github.com>
2018-06-24 11:27:06 +0200

    first commit

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+        // put your code here
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Register</title>
+    </head>
+    <body>
+        <div id="wrapper">
+            <form action="#" method="POST">
+                <p><label>First Name</label><input type="text" name="firstname" value="" /></p>
+                <p><label>Last Name</label><input type="text" name="lastname" value="" /></p>
+                <p><label>E-mail/label><input type="text" name="email" value="" /></p>
+                <p><label>Password</label><input type="text" name="password" value="" /></p>
+                <p><label>Confirm Password</label><input type="text" name="password2" value="" /></p>
+                <p><input type="submit" name="Submit" value="" /></p>
+            </form>    
+        </div>
+    </body>
+</html>
